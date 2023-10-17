32302c8b685f93d83d8bbb2adc8a9a49a6f3818c
ak869346 <ak869346@gmail.com>
2023-10-17 12:40:05 +0530

    Adding Operators conecpts in JS

---
+++ Lecture6.js
@@ -0,0 +1,42 @@
+ // Arthmetic Operator
+
+ let a = 10;
+ let b = 4;
+
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(a/b);
+ console.log(a**b);
+ 
+
+ console.log("+aa = " + ++a);
+ console.log("a++ =" + a++);
+ console.log("a="+a);
+
+
+ // Assignement operator...
+
+ let d = 7;
+ console.log(d);
+
+ d +=10;
+ console.log(d);
+
+
+ let e = 8;
+ let f = "8";
+
+ console.log("e==f" ,e==f);  // do not check data type of data...
+ console.log("e===f", e===f);  // check data type as well
+
+
+ // Logical Operator
+
+ let g = 23;
+ let h = 78;
+
+ console.log(g==23 && g<h);  // Both cond need to be true
+ console.log(g==23 || g<h);  // Only one cond need to be true for the result to be true...
+ console.log(!true);
+ console.log(!false);

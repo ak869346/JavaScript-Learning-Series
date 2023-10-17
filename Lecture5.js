efaeebfe0fa638795fe28a659f1ee10ca0713877
ak869346 <ak869346@gmail.com>
2023-10-17 12:08:56 +0530

    Adding practice set 1

---
+++ Lecture5.js
@@ -0,0 +1,37 @@
+// Ques 1
+let s = "Ankit";
+let a = 6;
+console.log(s+a);
+
+// Ques 2 
+
+console.log(typeof s);
+console.log(typeof a);
+
+// Ques 3 & 4
+
+let a1 = {
+    "name":"Vikash",
+    "city":"Dehri",
+    "contact":45
+}
+
+console.log(a1);
+
+a1["brother"] = "Sonu";
+
+console.log(a1);
+
+
+// Ques 5
+
+let dict = {
+    "Apple":"Sev",
+    "Man":"AAdmi",
+    "Women":"Aurat",
+    "People":"Log",
+    "Country":"Desh"
+}
+
+console.log(dict["Apple"]);
+console.log(dict["People"]);

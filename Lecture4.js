5d13f9782edb4b74cfd1687a9368ae50b2d2e99e
ak869346 <ak869346@gmail.com>
2023-10-17 11:50:42 +0530

    Adding Primitive data types and object in JS

---
+++ Lecture4.js
@@ -0,0 +1,27 @@
+// NN SS BB U - Data type shortcut
+
+let a = "null";
+let b = 123;
+let c = "Ankit";
+let d = Symbol("Chirag");
+let e = true;
+let f = BigInt("567");
+let g = undefined;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+
+
+let student = {
+    "name": "Virat",
+    "Tshirt no": 18,
+    "team": "India"
+}
+
+console.log(student["name"]);
+console.log(student["Tshirt no"]);

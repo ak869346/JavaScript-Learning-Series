b6e5c262b8980776ba6b4f6666deb6449bdc022f
ak869346 <ak869346@gmail.com>
2023-10-17 11:33:41 +0530

    Adding Lecture 2 and 3

---
+++ Lecture2and3.js
@@ -0,0 +1,21 @@
+var a = 9;
+// var b = "Prashant";
+var c = "Sammmy";
+
+var a = "Virat";
+
+let b = "Harry";
+b = "Shradha";
+console.log(b);
+
+{
+    let b = "this";
+    console.log(b);
+}
+
+const d = "Chirag";
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);

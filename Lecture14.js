ca2f50a11b392f24476a8f412ee1b204fdb5666b
ak869346 <ak869346@gmail.com>
2023-10-19 12:10:50 +0530

    String methods

---
+++ Lecture14.js
@@ -0,0 +1,20 @@
+let name = "Vikash\"Jee";
+console.log(name);
+console.log(name.length);
+
+
+let str = "Virat";
+let str1 = "Virat Kohli";
+let friend = "Rohit";
+let dost = "      Abhi          ";
+console.log(str.length)  // length is a property
+console.log(str.toUpperCase());  // toUpperCase is a method;
+console.log(str.toLowerCase());
+console.log(str.slice(2,4));
+console.log(str.slice(2));
+console.log(str1.replace("Kohli","Bhai"));
+console.log(str.concat(" is a friend of ",friend));
+console.log(dost);
+console.log(dost.trim());
+
+// String are immutable...

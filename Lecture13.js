6615c94cd7b259802d287a64b498f71d2b184cee
ak869346 <ak869346@gmail.com>
2023-10-19 11:44:55 +0530

    String Intro

---
+++ Lecture13.js
@@ -0,0 +1,24 @@
+let name = "Vikash";
+console.log(name);
+console.log(name.length);
+
+let friend = 'Vikash jee';
+console.log(friend);
+console.log(friend.length);
+console.log(friend[0]);
+
+// Template literals..
+
+let boy1 = "Sukanya";
+let boy2 = "Amit";
+
+let sent = `${boy2} is the husband of ${boy1}`;
+console.log(sent);
+
+// Escape Sequence 
+
+let fruit = 'Bana\'na';
+console.log(fruit);
+
+let fruit2 = "BANA\"NA";
+console.log(fruit2);

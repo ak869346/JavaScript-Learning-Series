943466de66e61848e1a8f55cdb07bee1f50df05a
ak869346 <ak869346@gmail.com>
2023-10-24 15:57:00 +0530

    Adding DOM Manipulation methods

---
+++ Lecture36.js
@@ -0,0 +1,15 @@
+document.getElementById();
+document.querySelector();
+document.querySelectorAll();
+document.getElementsByClassName();
+document.getElementsByName
+document.getElementsByTagName();
+
+
+// Searching the DOM Methods...
+
+// Three more methods...
+
+matches();
+closest();
+contain();
